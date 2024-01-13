9d2201ff21258b9c1a2e0369b36c7ac85dfcc4f2
Irving <irvingllerena@gmail.com>
2024-01-13 00:12:35 -0500

    agrego variables de entorno

--- init.py
+++ init.py
@@ -1,12 +1,15 @@
+import os
 import pandas as pd
 import pyodbc
 import sqlalchemy
 from sqlalchemy import create_engine
-
-servidor    ='DESKTOP-56AJ9RM\SQLSERVER'
-nombre_db   ='CONTROLMUNICIPAL'
-usuario     ='sa'
-password    ='123'
+from dotenv import load_dotenv
+ 
+load_dotenv()
+servidor    = os.getenv("Servidor")
+nombre_db   =os.getenv("nombre_db")
+usuario     =os.getenv("Usuario")
+password    = os.getenv("Password")
 
 engine = create_engine("mssql+pyodbc://"+usuario+":"+password+"@"+servidor+"/"+nombre_db+"?driver=ODBC+Driver+17+for+SQL+Server")
 con=engine.connect()
